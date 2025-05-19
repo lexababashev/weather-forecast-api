071d73c10bb926d48366415e21590fd639e6c3b1
Alex <alex.babashev@gmail.com>
2025-05-20 00:15:41 +0300

    init weather and sub points

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { WeatherModule } from './weather/weather.module';
+import { SubscriptionModule } from './subscription/subscription.module';
+
 
 @Module({
-  imports: [ConfigModule, PrismaModule],
+  imports: [ConfigModule, PrismaModule, WeatherModule, SubscriptionModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  app.setGlobalPrefix('api');
+  
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  
+  console.log(`Weather Forecast API is running on http://localhost:${port}/api`);
 }
 bootstrap();
